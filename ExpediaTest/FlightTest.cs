6455115d02c101a4ae78bf68c753b808ecb9088e
budocf <budocf@.rose-hulman.edu>
2015-03-19 12:40:04 -0400

    Commit of Expedia Tests

--- ExpediaTest/FlightTest.cs
+++ ExpediaTest/FlightTest.cs
@@ -8,5 +8,56 @@ namespace ExpediaTest
 	public class FlightTest
 	{
 		//TODO Task 7 items go here
+        private Flight target;
+        private readonly DateTime StartDate = new DateTime(2009, 11, 1);
+        private readonly DateTime EndDate = new DateTime(2009, 11, 30);
+
+        [SetUp()]
+        public void Setup()
+        {
+            target = new Flight(StartDate, EndDate, 10);
+        }
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            Assert.AreEqual(10, target.Miles);
+        }
+        [Test()]
+        public void TestOverideEquals()
+        {
+            Assert.True(target.Equals((object)target));
+        }
+        [Test()]
+        public void TestFlightEquals()
+        {
+            Assert.True(target.Equals(new Flight(StartDate,EndDate,10)));
+        }
+        [Test()]
+        public void TestFlightNotEquals()
+        {
+            Assert.False(target.Equals(new Flight(StartDate, EndDate, 15)));
+        }
+        [Test()]
+        public void TestBasePrice()
+        {
+            Assert.AreEqual(target.getBasePrice().CompareTo(780),0);
+        }
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatFlightThrowsOnBadLength()
+        {
+            new Flight(StartDate,EndDate,-5);
+        }
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestThatFlightThrowsOnIncorrectDate()
+        {
+            new Flight(EndDate, StartDate, 5);
+        }
+        [TearDown()]
+        public void TearDown()
+        {
+            target = null; // this is entirely unnecessary.. but I'm just showing a usage of the TearDown method here
+        }
 	}
 }
